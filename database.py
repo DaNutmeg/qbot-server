5712d59c6e3f0ea12b807bf1ab0d296511335730
SkyNotion <meshdrew6@gmail.com>
2026-02-06 18:44:38 +0100

    Updated database code to use valkey and misc

--- database.py
+++ database.py
@@ -8,12 +8,14 @@ from sqlalchemy.ext.asyncio import (
     async_sessionmaker,
     AsyncSession
 )
-from sqlalchemy import Double, String, ForeignKey, select
+from sqlalchemy import Integer, Double, String, ForeignKey, select
 from sqlalchemy.orm import (
     DeclarativeBase,
     Mapped,
     mapped_column
 )
+import aiosqlite
+import valkey.asyncio as valkey
 
 ENUM_NAIRA = "NGN"
 ENUM_DOLLAR = "USD"
@@ -23,6 +25,9 @@ ENUM_BUY = "BUY"
 
 ENUM_OPEN = "OPEN"
 ENUM_CLOSED = "CLOSED"
+
+ENUM_PENDING = "PENDING"
+ENUM_RUNNING = "RUNNING"
 
 class Base(DeclarativeBase):
     pass
@@ -49,9 +54,23 @@ class Trades(Base):
     stock_id: Mapped[int] = mapped_column(ForeignKey("stocks.id"))
     order_type: Mapped[str] = mapped_column(nullable=False)
     status: Mapped[str] = mapped_column(nullable=False)
-    value: Mapped[float] = mapped_column(default=0.0)
+    amount: Mapped[float] = mapped_column(default=1.0)
+    price: Mapped[float] = mapped_column(default=0.0)
+    pnl: Mapped[float] = mapped_column(default=0.0)
     created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
     updated_at: Mapped[datetime] = mapped_column(default=datetime.utcnow, onupdate=datetime.utcnow)
+
+class Orders(Base):
+    __tablename__ = "orders"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    account_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("accounts.id"))
+    stock_id: Mapped[int] = mapped_column(ForeignKey("stocks.id"))
+    stop_loss: Mapped[float] = mapped_column(default=200.0)
+    take_profit: Mapped[float] = mapped_column(default=800.0)
+    amount: Mapped[float] = mapped_column(default=0.0)
+    duration: Mapped[str] = mapped_column(default="15m")
+    status: Mapped[str] = mapped_column(nullable=False)
 
 load_dotenv()
 
@@ -61,18 +80,21 @@ DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_NAME = os.getenv("DB_NAME", "qbot")
 
 DB_CONN_STRING = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-STOCKS_DB_CONN_STRING = f"sqlite+aiosqlite://{os.getenv("STOCKS_DB")}"
+VALKEY_DB_CONN_STRING = f"valkey://{os.getenv("VALKEY_HOST")}"
+VALKEY_DB_CONN_STRING = f"{os.getenv("VALKEY_HOST")}"
 
 class Database():
 
     def __init__(self):
         self.db_engine = None
-        self.stocks_db_engine = None
+        self.stocks_db = None
+        self.valkey_conn_str = VALKEY_DB_CONN_STRING
 
     async def init(self):
        self.db_engine = create_async_engine(DB_CONN_STRING)
        async with self.db_engine.begin() as conn:
            await conn.run_sync(Base.metadata.create_all)
+       self.stocks_db = await aiosqlite.connect(os.getenv("STOCKS_DB"))
 
     async def session(self):
         async_session = async_sessionmaker(self.db_engine, expire_on_commit=False)
@@ -80,8 +102,12 @@ class Database():
             async with asession.begin():
                 yield asession
 
+    async def valkey_session(self):
+        return valkey.Valkey.from_url(self.valkey_conn_str, socket_timeout=1800, decode_responses=True)
+
     async def close(self):
         await self.db_engine.dispose()
+        await self.stocks_db.close()
 
     async def get_account_session(self, session_id: str | None, asession: AsyncSession):
         account = None
@@ -89,3 +115,10 @@ class Database():
             account = await asession.execute(select(Accounts).where(Accounts.id == uuid.UUID(session_id)))
             account = account.scalars().first()
         return account
+
+    async def add_new_trade(self, data, session):
+        session.add(Trades(
+            id=uuid.uuid4(),
+            **data
+        ))
+        print(f"add_new_trade - Added new trade {data}")
